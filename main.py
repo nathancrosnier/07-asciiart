401c3d1524ed591516bb0b9aee937ee98934febc
nathancrosnier <nathan.crosnier@edu.esiee.fr>
2024-11-28 09:16:39 +0000

    Exercice 7

--- main.py
+++ main.py
@@ -1,49 +1,53 @@
 #### Imports et définition des variables globales
-
-
+"""AAAAAAAA"""
+import sys
+sys.setrecursionlimit(10000)
 #### Fonctions secondaires
 
-
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
-
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+    selon un algorithme itératif
     Args:
         s (str): la chaîne de caractères à encoder
-
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
-
-    return [ ]
-
+    occ = []
+    cmpt = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            cmpt += 1
+        else:
+            occ.append((s[i-1],cmpt))
+            cmpt = 1
+    occ.append((s[-1], cmpt))
+    return occ
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
-
+    """retourne la liste de tuples encodant une chaîne de caractères passée 
+    en argument selon un algorithme récursif
     Args:
         s (str): la chaîne de caractères à encoder
-
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
+    if not s:
+        return []
+    cmpt = 1
+    while cmpt < len(s) and s[0] == s[cmpt]:
+        cmpt += 1
+    return [(s[0], cmpt)] + artcode_r(s[cmpt:])
 
 #### Fonction principale
 
-
 def main():
-    print(artcode_i('MMMMaaacXolloMM'))
-    print(artcode_r('MMMMaaacXolloMM'))
+    """
+    Tests
+    """
+    print(artcode_i("AAABBBCCCABC"))
+    print(artcode_r("AAABBBCCCABC"))
+    print(artcode_r("gggfhhhhhhggg"))
 
 if __name__ == "__main__":
     main()
